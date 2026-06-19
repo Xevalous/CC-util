2df870228828866e274a1b52e97ecd3cf967b06e
Xevalous <helmiaryantoo+git@gmail.com>
2026-06-20 02:03:18 +0700

    feat: add versioning and release workflow

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+var version = "dev"
+
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Printf("cc-util %s\n", version)
+		os.Exit(0)
+	}
+
 	p := tea.NewProgram(ui.NewPrecheck())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
